acf65a88995c0915a4b161593a0431f87b2a02dc
nerdchacha <yatin.gera5@gmail.com>
2021-12-15 19:18:03 +0530

    Added auto test changes to first javascript file

--- code-samples/voice/call-log.js
+++ code-samples/voice/call-log.js
@@ -1,38 +1,46 @@
-const RC = require('@ringcentral/sdk').SDK
-require('dotenv').config();
+const RC = require('@ringcentral/sdk').SDK;
 
-CLIENTID     = process.env.RC_CLIENT_ID
-CLIENTSECRET = process.env.RC_CLIENT_SECRET
-SERVER       = process.env.RC_SERVER_URL
-USERNAME     = process.env.RC_USERNAME
-PASSWORD     = process.env.RC_PASSWORD
-EXTENSION    = process.env.RC_EXTENSION
+const CLIENTID     = process.env.RC_CLIENT_ID;
+const CLIENTSECRET = process.env.RC_CLIENT_SECRET;
+const SERVER       = process.env.RC_SERVER_URL;
+const USERNAME     = process.env.RC_USERNAME;
+const PASSWORD     = process.env.RC_PASSWORD;
+const EXTENSION    = process.env.RC_EXTENSION;
 
-var rcsdk = new RC({
+const rcsdk = new RC({
     server:       SERVER,
     clientId:     CLIENTID,
     clientSecret: CLIENTSECRET
 });
-var platform = rcsdk.platform();
+
+const platform = rcsdk.platform();
+
 platform.login({
     username:  USERNAME,
     password:  PASSWORD,
     extension: EXTENSION
-})
-
-platform.on(platform.events.loginSuccess, () => {
-  read_user_calllog()
 });
 
-async function read_user_calllog() {
-  try {
-    var resp = await platform.get('/account/~/extension/~/call-log', {
-      view: 'Detailed'
-    })
-    var jsonObj = await resp.json()
-    for (var record of jsonObj.records)
-      console.log("Call type: " + record.type)
-  } catch (e) {
-    console.log(e.message)
-  }
+platform.on(platform.events.loginSuccess, readUserCallLog);
+
+platform.on(platform.events.loginError, (e) => {
+    console.error(`User login failed : ${e.message}`);
+    // Remove the below line if you are running this in the browser
+    process.exit(1);
+});
+
+async function readUserCallLog() {
+    try {
+        const response = await platform.get('/restapi/v1.0/account/~/extension/~/call-log', {
+            view: 'Detailed'
+        });
+        const json = await response.json();
+        for (let record of json.records) {
+            console.log("Call type: " + record.type);
+        }
+    } catch (e) {
+        console.error(`Failed to read detailed call logs : ${e.message}`);
+        // Remove the below line if you are running this in the browser
+        process.exit(1);
+    }
 }
