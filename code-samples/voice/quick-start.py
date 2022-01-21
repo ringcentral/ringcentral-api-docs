f42397d2a1eac7c88e5e149f594a11ec61ae1043
byrne.reese <byrne@majordojo.com>
2022-01-21 12:17:01 -0800

    Fixed python code sample for voice

--- code-samples/voice/quick-start.py
+++ code-samples/voice/quick-start.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from ringcentral import SDK
 import os,sys
+from dotenv import load_dotenv
+load_dotenv()
 
-RECIPIENT    = os.environ.get('RINGOUT_RECIPIENT')
+RECIPIENT = os.environ.get('RINGOUT_RECIPIENT')
 
 rcsdk = SDK( os.environ.get('RC_CLIENT_ID'),
              os.environ.get('RC_CLIENT_SECRET'),
@@ -12,8 +14,8 @@ try:
   platform.login(os.environ.get('RC_USERNAME'),
                  os.environ.get('RC_EXTENSION'),
                  os.environ.get('RC_PASSWORD') )
-except:
-  sys.exit("Unable to authenticate to platform. Check credentials.")
+except Exception as e:
+  sys.exit("Unable to authenticate to platform: " + str(e))
 
 resp = platform.post('/restapi/v1.0/account/~/extension/~/ring-out',
               {
