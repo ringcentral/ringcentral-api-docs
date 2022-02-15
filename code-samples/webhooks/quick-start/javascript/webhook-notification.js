6b34efba876f10d45d0533666e7b7d5a6abd0947
singhgupi <58199964+singhgupi@users.noreply.github.com>
2022-02-15 17:09:02 -0500

    Adding 'Content-type', 'application/json' for py and js webhook code samples
    Adding 'Content-type', 'application/json' for py and js webhook code samples.

--- code-samples/webhooks/quick-start/javascript/webhook-notification.js
+++ code-samples/webhooks/quick-start/javascript/webhook-notification.js
@@ -17,6 +17,7 @@ var server = http.createServer(function(req, res) {
   if (req.method == 'POST') {
     if (req.url == "/webhook") {
       if (req.headers.hasOwnProperty("validation-token")) {
+        res.setHeader('Content-type', 'application/json');
         res.setHeader('Validation-Token', req.headers['validation-token']);
         res.statusCode = 200;
         res.end();
