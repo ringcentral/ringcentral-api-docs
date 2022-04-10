dc42bc9f49bf8fd1b2424c42d33e2be476dae3e5
Neil Xie <rebom8888@gmail.com>
2022-04-10 20:57:39 +0800

    Updated sample, now just set usePKCE to true
    LoginUrlOptions now provides usePKCE (boolean) attribute, so that developers do not need to generate and set `code_verifier` and `code_challenge`.
    please see 👉 https://github.com/ringcentral/ringcentral-js/blob/ed5a31004e623cfda189eced0de5fb025cb9263d/sdk/src/platform/Platform.ts#L282

--- code-samples/auth/login-url-pkce.js
+++ code-samples/auth/login-url-pkce.js
@@ -1,25 +1,4 @@
 const RC = require('@ringcentral/sdk').SDK
-import {randomBytes} from 'crypto';
-import {createHash} from 'crypto';
-
-function _generateCodeVerifier() {
-    let codeVerifier: any = randomBytes(32);
-    codeVerifier = codeVerifier
-	.toString('base64')
-	.replace(/\+/g, '-')
-	.replace(/\//g, '_')
-	.replace(/=/g, '');
-    return codeVerifier;
-}
-
-let codeVerifier = this._generateCodeVerifier();
-let codeChallenge = createHash('sha256')
-    .update(codeVerifier)
-    .digest()
-    .toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '');
 
 var rcsdk = new RC({
     'server':       process.env.RC_SERVER_URL,
@@ -31,6 +10,5 @@ var platform = rcsdk.platform();
 
 console.log( "Login URL: ", platform.loginUrl({
     "state": "1234567890",
-    "code_challenge": codeChallenge,
-    "code_challenge_method": "S256"
+    "usePKCE": true
 }) )
