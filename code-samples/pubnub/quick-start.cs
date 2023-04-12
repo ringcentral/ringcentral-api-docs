79e5f5bb71bfaf8415483684afbf7e32bf48932d
byrne.reese <byrne@majordojo.com>
2023-04-12 11:56:29 -0700

    Fixed C# sample code for pubnub;

--- code-samples/pubnub/quick-start.cs
+++ code-samples/pubnub/quick-start.cs
@@ -1,52 +1,49 @@
-package PubNub_Notifications;
+using System;
+using System.Threading.Tasks;
+using RingCentral;
 
-import java.io.IOException;
-
-import com.ringcentral.*;
-import com.ringcentral.definitions.*;
-import com.ringcentral.pubnub.Subscription;
-
-public class PubNub_Notifications {
+namespace PubNub_Notifications
+{
+class Program
+{
     static RestClient restClient;
-
-    public static void main(String[] args) {
-        try {
-            PubNubNotifications();
-        } catch (RestException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void PubNubNotifications() throws RestException, IOException {
+    static void Main(string[] args)
+    {
 	restClient = new RestClient(
 	    Environment.GetEnvironmentVariable("RC_CLIENT_ID"),
 	    Environment.GetEnvironmentVariable("RC_CLIENT_SECRET"),
 	    Environment.GetEnvironmentVariable("RC_SERVER_URL"));
 	restClient.Authorize(
 	    Environment.GetEnvironmentVariable("RC_JWT")).Wait();
-
-        var eventFilters = new String[] {
-            "/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS"
-        };
-        Subscription subscription = new Subscription(restClient, eventFilters, (message)->{
-            var gs = new Gson();
-            if (message.contains("instant?type=SMS")) {
-                InstantMessageEvent notification = gs.fromJson( message, InstantMessageEvent.class);
-                InstantMessageEventBody body = notification.body;
-                System.out.println(body.subject);
-            }
-        });
-
-        subscription.subscribe();
-        System.out.println("Ready to receive incoming SMS via PubNub.");
-
-        try {
+        pubnub_notification().Wait();
+    }
+    static private async Task pubnub_notification()
+    {
+        try
+        {
+            var eventFilters = new[]
+            {
+                "/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS"
+            };
+            var subscription = new Subscription(rcsdk, eventFilters, message =>
+                {
+                    var jsonObj = JObject.Parse(message);
+                    if (jsonObj["event"].ToString().Contains("instant?type=SMS"))
+                    {
+                        Console.WriteLine(jsonObj["body"]["subject"]);
+                    }
+                });
+            var subscriptionInfo = await subscription.Subscribe();
+            Console.WriteLine("Ready to receive incoming SMS via PubNub.");
             while (true)
             {
-                Thread.sleep(10000);
+                Thread.Sleep(5000);
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
         }
     }
+}
 }
