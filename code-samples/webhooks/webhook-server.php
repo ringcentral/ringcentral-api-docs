4fe92c74789afcfff596571ead951ff4ba263238
Michael Stowe <me@mikestowe.com>
2023-06-17 15:06:43 -0700

    Update webhook-server.php
    Updated for consistency with other webhook PHP example

--- code-samples/webhooks/webhook-server.php
+++ code-samples/webhooks/webhook-server.php
@@ -1,8 +1,9 @@
 <?php
-if (isset($_SERVER['HTTP_VALIDATION_TOKEN'])) { 
-    header("Validation-Token:: " . $_SERVER['HTTP_VALIDATION_TOKEN']);
+$v = isset($_SERVER['HTTP_VALIDATION_TOKEN']) ? $_SERVER['HTTP_VALIDATION_TOKEN'] : '';
+if (strlen($v) > 0) {
+    header("Validation-Token: {$v}");
 }
-            
+
 $jsonStr = file_get_contents('php://input');
 $jsonObj = json_decode($jsonStr, TRUE);
 var_dump($jsonObj);
