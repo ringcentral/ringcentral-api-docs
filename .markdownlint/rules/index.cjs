f2dcafbd5ce48e388fcb5580afb598873a566bba
vincent.cai <vincent.cai@ringcentral.com>
2023-06-25 19:44:08 +0800

    Update Markdown Linter

--- .markdownlint/rules/index.cjs
+++ .markdownlint/rules/index.cjs
@@ -1,12 +1,3 @@
-module.exports = process.env.NODE_ENV === 'cli' ? {
-  "default": false,
-  "DPW001": true,
-  "DPW002": true,
-  "DPW003": true,
-  "MD013": false,
-} : {
-  "default": true,
-  "MD013": false,
-  "MD033": false,
-  "DPW003": true,
-};
+module.exports = process.env.NODE_ENV === 'cli' 
+  ? require('./ci.cjs') 
+  : require('./vscode.cjs');
