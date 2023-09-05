379a278bd01f8f693b8c8177a9b2cd58e7a884e1
vincent.cai <vincent.cai@ringcentral.com>
2023-09-06 00:06:00 +0800

    Linter: New Rule (DPW005) for Code Samples

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -5,5 +5,6 @@ module.exports = {
     './.markdownlint/customRules/rules/md-images',
     './.markdownlint/customRules/rules/html-links',
     './.markdownlint/customRules/rules/md-links',
+    './.markdownlint/customRules/rules/code-samples', 
   ]
 };
