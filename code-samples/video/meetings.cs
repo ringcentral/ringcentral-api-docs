afe4a2426bc848f4b8a1582571ba8c51aee90438
Paco Vu <paco_vu@yahoo.com>
2024-06-05 13:59:12 -0700

    New dev guide and fixed items
    - Added SMS template dev guide
    - Fixed webhooks sample code local port
    - Fixed video meeting sample code for all languages.

--- code-samples/video/meetings.cs
+++ code-samples/video/meetings.cs
@@ -1,46 +1,45 @@
 using System;
 using RingCentral;
+
 namespace CreateMeeting
-
 {
-    class Program
+  class Program
+  {
+    static RestClientrest Client;
+    static async Task Main()
     {
-        static RestClientrest Client;
-        static async Task Main()
-        {
-            try
+      try
+      {
+        DotEnv.Load();
+        // Instantiate the SDK
+        restClient = new RestClient(Environment.GetEnvironmentVariable("RC_CLIENT_ID"),
+                                    Environment.GetEnvironmentVariable("RC_CLIENT_SECRET"),
+                                    Environment.GetEnvironmentVariable("RC_SERVER_URL"));
 
-            {
-                DotEnv.Load();
-                // Instantiate the SDK 
-                restClient = new RestClient(Environment.GetEnvironmentVariable("RC_CLIENT_ID"), Environment.GetEnvironmentVariable("RC_CLIENT_SECRET"), Environment.GetEnvironmentVariable("RC_SERVER_URL"));
-                // Authenticate a user using a personal JWT token
-                await restClient.Authorize(Environment.GetEnvironmentVariable("RC_JWT"));
-                await createMeeting();
-            }
-
-            catch (Exceptione e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-        }
-        /*
-        Create a new meeting with specific parameters
-        */
-        private static async Task createMeeting()
-        {
-            try
-            {
-                var createBridgeRequest = new CreateBridgeRequest { name = "MyMeeting", type = "Scheduled", };
-                BridgeResponse bridgeResponse = await restClient.Rcvideo().V2().Account("~").Extension("~").Bridges().Post(createBridgeRequest);
-                Console.WriteLine("Meeting Created Id "bridgeResponse.id" Name "bridgeResponse.name" type " + bridgeResponse.type);
-            }
-            catch (Exceptione e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-        }
+        // Authenticate a user using a personal JWT token
+        await restClient.Authorize(Environment.GetEnvironmentVariable("RC_JWT"));
+        await create_meeting();
+      }
+      catch (Exceptione e)
+      {
+        Console.WriteLine(e.Message);
+      }
     }
+    /*
+    Create a an instant RCV meeting
+    */
+    private static async Task create_meeting()
+    {
+      try
+      {
+        var bodyParams = new CreateBridgeRequest { name = "Test meeting", type = "Instant" };
+        var response = await restClient.Rcvideo().V2().Account("~").Extension("~").Bridges().Post(bodyParams);
+        Console.WriteLine("Start Your Meeting:  " + response.discovery.web);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+  }
 }
